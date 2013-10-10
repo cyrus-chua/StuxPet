29d22bfc4f8058bf3f5a5d5248b0f3d41b465e76
languan <languantan@gmail.com>
2013-10-10 19:37:36 +0800

    added bulk of phonegap into app

---
+++ phonegap/platforms/android/gen/com/stuxpair/stuxpet/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.stuxpair.stuxpet;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ phonegap/platforms/android/src/com/stuxpair/stuxpet/StatsTrackerService.java
@@ -0,0 +1,51 @@
+package com.stuxpair.stuxpet;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class StatsTrackerService extends IntentService {
+
+	private int stepsTaken = 0;
+
+	/**
+	 * A constructor is required, and must call the super IntentService(String)
+	 * constructor with a name for the worker thread.
+	 */
+	public StatsTrackerService() {
+		super("StatsTrackerService");
+	}
+
+	/**
+	 * The IntentService calls this method from the default worker thread with
+	 * the intent that started the service. When this method returns,
+	 * IntentService stops the service, as appropriate.
+	 */
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// Normally we would do some work here, like download a file.
+		// For our sample, we just sleep for 5 seconds.
+		startStepsCounter();
+		long endTime = System.currentTimeMillis() + 20 * 1000;
+		while (System.currentTimeMillis() < endTime) {
+			synchronized (this) {
+				try {
+					wait(endTime - System.currentTimeMillis());
+				} catch (Exception e) {
+				}
+			}
+		}
+	}
+
+	public void startStepsCounter() {
+		stepsTaken++;
+	}
+
+	public int getStepsTaken() {
+		return stepsTaken;
+	}
+
+	public void setStepsTaken(int stepsTaken) {
+		this.stepsTaken = stepsTaken;
+	}
+
+}
