6006057121cd290e30af563a1941534903750996
languan <languantan@gmail.com>
2013-10-10 22:59:37 +0800

    fucked up innerwidth and height.

---
+++ phonegap/platforms/android/assets/www/js/setup.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var wallpaper, text;
+	wallpaper = document.getElementById('wallpaper');
+	wallpaper.width = window.innerWidth;
+	wallpaper.height = window.innerHeight;
+};
