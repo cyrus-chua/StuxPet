92923f5e9c506074d8261e0e95355ec56e91ba81
languan <languantan@gmail.com>
2013-11-09 12:48:55 +0800

    smoother transition
    used div instead of canvas

--- phonegap/platforms/android/assets/www/js/setup.js
+++ phonegap/platforms/android/assets/www/js/setup.js
@@ -1,10 +1,62 @@
-var wallpaper, ctx, door = 0, seq = 0;
+var sliding = startClientX = startPixelOffset = pixelOffset = currentSlide = 0,
+ slideCount = document.getElementsByClassName('door').length;
+
+window.addEventListener('mousedown touchstart', slideStart);
+window.addEventListener('mouseup touchend', slideEnd);
+window.addEventListener('mousemove touchmove', slide);
+
+function slideStart(event) {
+    if (event.originalEvent.touches)
+      event = event.originalEvent.touches[0];
+    if (sliding == 0) {
+      sliding = 1;
+      startClientX = event.clientX;
+    }
+  }
+
+ function slide(event) {
+    event.preventDefault();
+    if (event.originalEvent.touches)
+      event = event.originalEvent.touches[0];
+     var deltaSlide = event.clientX - startClientX;
+
+if (sliding == 1 && deltaSlide != 0) {
+      sliding = 2;
+      startPixelOffset = pixelOffset;
+    }
+
+if (sliding == 2) {
+      var touchPixelRatio = 1;
+      if ((currentSlide == 0 && event.clientX > startClientX) ||
+          (currentSlide == slideCount - 1 && event.clientX < startClientX))
+        touchPixelRatio = 3;
+      pixelOffset = startPixelOffset + deltaSlide / touchPixelRatio;
+      $('#doors').css('transform', 'translate3d(' + pixelOffset + 'px,0,0)').removeClass();
+    }
+  }
+
+function slideEnd(event) {
+    if (sliding == 2) {
+      sliding = 0;
+      currentSlide = pixelOffset < startPixelOffset ? currentSlide + 1 : currentSlide - 1;
+      currentSlide = Math.min(Math.max(currentSlide, 0), slideCount - 1);
+      pixelOffset = currentSlide * -$('body').width();
+      document.getElementById('#temp').remove();
+      ('<style id="temp">#doors.animate{transform:translate3d(' + pixelOffset + 'px,0,0)}</style>').appendTo('head');
+      document.getElementById('doors').addClass('animate').css('transform', '');
+    }
+ }
+
+/*var wallpaper, ctx, door = 0, seq = 0;
 var nav = {};
 for ( var i = 0; i < 7; i++) {
 	var pic = new Image();
 	pic.src = "img/nav" + i + ".png";
 	nav[i] = pic;
 }
+*/
+
+//init animation images
 var openAnimate = {};
 for ( var i = 0; i < 1; i++) {
 	openAnimate[i] = {};
@@ -15,6 +67,8 @@ for ( var i = 0; i < 1; i++) {
 	}
 }
 
+/*
+//get access to all elements
 function init() {
 	wallpaper = document.getElementById('wallpaper');
 	ctx = wallpaper.getContext("2d");
@@ -24,6 +78,7 @@ function init() {
 	setTimeout(paintWallpaper, 5000);
 }
 
+//loading page
 function loading() {
 	splash.innerHTML += ".";
 	if (splash.style.display != 'none') {
@@ -90,3 +145,4 @@ function openAnimation() {
 if (!window.onload) {
 	window.onload = init;
 }
+*/
